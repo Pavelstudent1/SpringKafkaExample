34e98232e313e083b50447a94f000944d0d79c80
Pavel Fedorov <Pavel_Fedorov@epam.com>
2020-07-25 03:06:01 +0300

    Minimal setup for Kafka working

---
+++ producer/src/main/java/example/service/Consumer.java
@@ -0,0 +1,17 @@
+package example.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+    private static final Logger LOG = LoggerFactory.getLogger(Consumer.class);
+
+    @KafkaListener(topics = "users", groupId = "group_id")
+    public void consume(String message) {
+        LOG.info(">>> Consumed Message -> {}", message);
+    }
+
+}
---
+++ producer/src/main/java/example/service/Producer.java
@@ -0,0 +1,28 @@
+package example.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
+    private static final String TOPIC = "users";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOG.info(">>> Producing message --> {}", message);
+        this.kafkaTemplate.send(TOPIC, message);
+
+    }
+
+}
